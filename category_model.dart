15b77aaf7ecbcaf3033015e04b220039e7092107
Van-Khang <125308490+Van-Khang@users.noreply.github.com>
2023-12-22 22:33:40 +0700

    Add files via upload

---
+++ category_model.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+CategoryModel CategoryModelFromJson(String str) =>
+    CategoryModel.fromJson(json.decode(str));
+
+String categoryModelToJson(CategoryModel data) => json.encode(data.toJson());
+
+class CategoryModel {
+  CategoryModel({
+    required this.image,
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.status,
+    required this.isFavourite,
+  });
+
+  String image;
+  String id;
+  bool isFavourite;
+  String name;
+  double price;
+  String description;
+  String status;
+  
+  factory CategoryModel.fromJson(Map<String, dynamic> json) => CategoryModel(
+      id: json["id"],
+      name: json["name"],
+      description: json["description"],
+      image: json["image"],
+      isFavourite: false,
+      price: double.parse(json["price"]),
+      status: json["status"],
+    );
+  
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "image": image,
+    "description": description,
+    "isFavourite": isFavourite,
+    "price": price,
+    "status": status,
+  };
+} 
